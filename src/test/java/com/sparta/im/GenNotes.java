288ef296a61a1c4c308deddca9b3ef73efab465f
RizwanSulaman <rizwansulaman@hotmail.co.uk>
2026-03-20 10:05:55 +0000

    notes

---
+++ src/test/java/com/sparta/im/GenNotes.java
@@ -0,0 +1,21 @@
+package com.sparta.im;
+
+public class GenNotes {
+
+   /* https://regex101.com/
+   https://saucelabs.com/
+   https://docs.oracle.com/en/java/javase/21/index.html
+   https://hamcrest.org/JavaHamcrest/javadoc/1.3/org/hamcrest/MatcherAssert.html
+   https://postcodes.io/endpoints/
+   https://mermaid.ai/web/
+   https://www.token-tailor.com/
+   https://www.youtube.com/@UniProgrammer
+   https://medium.com/@roy.mor/rest-api-test-strategy-what-exactly-should-you-test-21c2f1cc3ed5
+   https://www.youtube.com/watch?v=gell20XdEEE
+   https://serenity-bdd.github.io/
+   https://docs.github.com/en/actions/how-tos/write-workflows/use-workflow-templates
+   workflow
+   yaml
+
+    */
+}
